ca41e9718451e02b052bce165263dfea7a934aa2
Ksenia001 <kseniabelko01@gmail.com>
2025-03-03 09:38:30 +0300

    .

--- src/main/java/com/example/library/LibraryApplication.java
+++ src/main/java/com/example/library/LibraryApplication.java
@@ -11,3 +11,6 @@ public class LibraryApplication {
 	}
 
 }
+//http://localhost:8080/library/books
+//http://localhost:8080/library/book/1
+//http://localhost:8080/library/books?author=Joshua%20Bloch
